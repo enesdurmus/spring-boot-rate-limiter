2a4d331d57433c4c1463683e71cfe329b991087d
enes-durmus <enesdurmus3738@gmail.com>
2025-04-13 22:52:09 +0300

    init

---
+++ src/main/java/com/endu/throttler/SpringBootThrottlerApplication.java
@@ -0,0 +1,13 @@
+package com.endu.throttler;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootThrottlerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootThrottlerApplication.class, args);
+	}
+
+}
